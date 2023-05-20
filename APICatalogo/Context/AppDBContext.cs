4f9df0f581757b8baffcc0955ea28109cb4c6a52
thiagorazzini <razzini.thiago@gmail.com>
2023-05-20 18:47:42 -0300

    Arquivo dbcontext criado

---
+++ APICatalogo/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>?  Produtos { get; set; }
+    }
+}
