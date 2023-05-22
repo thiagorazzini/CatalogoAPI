3cd3f5c39bc333026a745eacf6728a72c7abaa4f
thiagorazzini <razzini.thiago@gmail.com>
2023-05-22 11:34:20 -0300

    Created Post method

--- APICatalogo/Migrations/ProdutosController.cs
+++ APICatalogo/Migrations/ProdutosController.cs
@@ -67,5 +67,19 @@ namespace APICatalogo.Migrations
             return Ok(produto);
 
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id) {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontado...");
+            }
+
+            _context.Produtos.Remove(produto); 
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
