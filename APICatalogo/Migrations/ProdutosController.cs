efa6b460cb712ce56dc92188dd3426b5c5cfde2a
thiagorazzini <razzini.thiago@gmail.com>
2023-05-22 10:19:01 -0300

    Criando controlador

---
+++ APICatalogo/Migrations/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Migrations
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public ProdutosController(AppDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
